d941f3b42e8e054be27c01730dee1b52a1b7d849
k1rakishou <kez1911@protonmail.com>
2023-04-22 13:06:18 +0700

    Better test it with multiple accounts.

--- src/tests/handlers/get_account_info_tests.rs
+++ src/tests/handlers/get_account_info_tests.rs
@@ -31,43 +31,84 @@ mod tests {
 
     async fn should_return_account_info_if_account_exists() {
         let user_id1 = &account_repository_shared::TEST_GOOD_USER_ID1;
+        let user_id2 = &account_repository_shared::TEST_GOOD_USER_ID2;
         let account_id1 = AccountId::from_user_id(user_id1).unwrap();
+        let account_id2 = AccountId::from_user_id(user_id2).unwrap();
         let database = database_shared::database();
 
         account_repository_shared::create_account_actual(
             user_id1
         ).await;
 
-        let server_response = account_repository_shared::get_account_info::<AccountInfoResponse>(
-            user_id1
-        ).await.unwrap();
+        account_repository_shared::create_account_actual(
+            user_id2
+        ).await;
 
-        assert!(server_response.data.is_some());
-        assert!(server_response.error.is_none());
+        {
+            let server_response = account_repository_shared::get_account_info::<AccountInfoResponse>(
+                user_id1
+            ).await.unwrap();
 
-        let account_info_response = server_response.data.unwrap();
-        assert_eq!(true, account_info_response.is_valid);
-        assert_eq!(false, account_info_response.valid_until.is_none());
+            assert!(server_response.data.is_some());
+            assert!(server_response.error.is_none());
 
-        let from_cache = account_repository_shared::get_account_from_cache(user_id1)
-            .await
-            .unwrap()
-            .unwrap();
+            let account_info_response = server_response.data.unwrap();
+            assert_eq!(true, account_info_response.is_valid);
+            assert_eq!(false, account_info_response.valid_until.is_none());
 
-        assert_eq!(1, from_cache.id_generated);
-        assert_eq!(account_id1.id, from_cache.account_id.id);
-        assert!(&from_cache.firebase_token().is_none());
-        assert!(&from_cache.valid_until.is_some());
+            let from_cache = account_repository_shared::get_account_from_cache(user_id1)
+                .await
+                .unwrap()
+                .unwrap();
 
-        let from_database = account_repository_shared::get_account_from_database(user_id1, database)
-            .await
-            .unwrap()
-            .unwrap();
+            assert_eq!(1, from_cache.id_generated);
+            assert_eq!(account_id1.id, from_cache.account_id.id);
+            assert!(&from_cache.firebase_token().is_none());
+            assert!(&from_cache.valid_until.is_some());
 
-        assert_eq!(1, from_database.id_generated);
-        assert_eq!(account_id1.id, from_database.account_id.id);
-        assert!(&from_database.firebase_token().is_none());
-        assert!(&from_database.valid_until.is_some());
+            let from_database = account_repository_shared::get_account_from_database(user_id1, database)
+                .await
+                .unwrap()
+                .unwrap();
+
+            assert_eq!(1, from_database.id_generated);
+            assert_eq!(account_id1.id, from_database.account_id.id);
+            assert!(&from_database.firebase_token().is_none());
+            assert!(&from_database.valid_until.is_some());
+        }
+
+        {
+            let server_response = account_repository_shared::get_account_info::<AccountInfoResponse>(
+                user_id2
+            ).await.unwrap();
+
+            assert!(server_response.data.is_some());
+            assert!(server_response.error.is_none());
+
+            let account_info_response = server_response.data.unwrap();
+            assert_eq!(true, account_info_response.is_valid);
+            assert_eq!(false, account_info_response.valid_until.is_none());
+
+            let from_cache = account_repository_shared::get_account_from_cache(user_id2)
+                .await
+                .unwrap()
+                .unwrap();
+
+            assert_eq!(2, from_cache.id_generated);
+            assert_eq!(account_id2.id, from_cache.account_id.id);
+            assert!(&from_cache.firebase_token().is_none());
+            assert!(&from_cache.valid_until.is_some());
+
+            let from_database = account_repository_shared::get_account_from_database(user_id2, database)
+                .await
+                .unwrap()
+                .unwrap();
+
+            assert_eq!(2, from_database.id_generated);
+            assert_eq!(account_id2.id, from_database.account_id.id);
+            assert!(&from_database.firebase_token().is_none());
+            assert!(&from_database.valid_until.is_some());
+        }
     }
 
 }
